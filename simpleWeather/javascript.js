da72f1a05650ed14b29926f0ea7baf334769fd81
Onebestever <carla.h1248@gmail.com>
2021-10-03 00:09:48 -0400

    i really enjoyed doing this 1st draft I thnk its perfect

---
+++ simpleWeather/javascript.js
@@ -0,0 +1,21 @@
+document.querySelector("#submit").addEventListener("click", weather)
+
+function weather(){
+    let city = document.querySelector('#city').value
+    let state = document.querySelector('#state').value
+    let country = document.querySelector('#country').value
+    let location = city + "," + state + "," + country
+    const url = ("https://api.openweathermap.org/data/2.5/weather?q=" + `${location}` + "&appid=399239f8b7aba56e5a6ed4831f09b2d7")
+
+fetch(url)
+.then(res => res.json()) 
+.then(data => {
+kelvinToFarenheit(data.main.temp)
+})
+}
+
+function kelvinToFarenheit(k){
+    let city = document.querySelector('#city').value
+    kToF = Math.floor((k - 273.15) * 9/5 + 32)
+    document.querySelector("#returnedTemp").innerText = 'The temperature in ' + city.toString() + ' is ' + kToF
+}
